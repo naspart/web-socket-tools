1192fcdf093c1589fb5af7ae5628f0a64811baac
Stephen Liu <stephen.cloud.liu@gmail.com>
2019-04-01 18:27:51 +0800

    更新包名

---
+++ src/main/java/com/yetoce/web_socket_tools/constant/WebSocketConstant.java
@@ -0,0 +1,8 @@
+package com.yetoce.web_socket_tools.constant;
+
+public class WebSocketConstant {
+    public final static String WEBSOCKET = "websocket";
+    public final static String UPGRADE = "Upgrade";
+    public final static String SEC_WEBSOCKET_PROTOCOL = "Sec-WebSocket-Protocol";
+    public final static String DEFAULT_WEBSOCKET_ADDRESS_FORMAT = "ws://%s";
+}
